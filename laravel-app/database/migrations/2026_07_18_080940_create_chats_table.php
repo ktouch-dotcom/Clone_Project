0ad328ba4553acfa70cc9033cd48dc62499b1f18
khun touch <k.touch@rua.edu.kh>
2026-07-21 16:33:17 +0700

    update all file

--- laravel-app/database/migrations/2026_07_18_080940_create_chats_table.php
+++ laravel-app/database/migrations/2026_07_18_080940_create_chats_table.php
@@ -4,16 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('chats', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('chats', function (Blueprint $table) {
+            $table->foreign('creator_id')
+                ->references('id')
+                ->on('users');
         });
     }
 
@@ -22,6 +22,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('chats');
+        Schema::table('chats', function (Blueprint $table) {
+            $table->dropForeign(['creator_id']);
+        });
     }
 };
